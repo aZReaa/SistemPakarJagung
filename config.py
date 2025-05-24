07eb9f6b3da9cae6748a7cb31d8ca16abee6aa5a
aZReaa <mynameisreza07@gmail.com>
2025-05-24 11:31:33 +0800

    first commit

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
+import os
+
 """
 Sistem Pakar Jagung Terpadu - Konfigurasi Aplikasi
 """
 
 # Konfigurasi aplikasi
-DEBUG = True
-KNOWLEDGE_BASE_PATH = "knowledge_base.json"
+DEBUG = True  # Consider setting to False for production
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))  # Define APP_ROOT
+KNOWLEDGE_BASE_PATH = os.path.join(APP_ROOT, "knowledge_base.json")  # Make path absolute
 
 # Konfigurasi logging
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 LOG_LEVEL = 'INFO'
-LOG_FILE = 'app.log'
+LOG_FILE = os.path.join(APP_ROOT, 'app.log')  # Make path absolute
 
 # Konfigurasi cache
 CACHE_TIMEOUT = 3600  # 1 jam dalam detik
